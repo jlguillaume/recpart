ce0e5fa81d9c62a3eb97c1a200c3b01656c7984a
Jean-Loup Guillaume <jean-loup.guillaume@univ-lr.fr>
2019-10-14 22:18:39 +0200

    add Louvain

--- main.c
+++ main.c
@@ -12,238 +12,246 @@
 
 //compute the maximum of three unsigned long
 inline unsigned long max3(unsigned long a,unsigned long b,unsigned long c){
-	a=(a>b) ? a : b;
-	return (a>c) ? a : c;
+  a=(a>b) ? a : b;
+  return (a>c) ? a : c;
 }
 
 //reading the edgelist from file
 adjlist* readedgelist(char* input){
-	unsigned long long e1=NLINKS;
-	adjlist *g=malloc(sizeof(adjlist));
-	FILE *file;
+  unsigned long long e1=NLINKS;
+  adjlist *g=malloc(sizeof(adjlist));
+  FILE *file;
 
-	g->n=0;
-	g->e=0;
-	file=fopen(input,"r");
-	g->edges=malloc(e1*sizeof(edge));
-	while (fscanf(file,"%lu %lu", &(g->edges[g->e].s), &(g->edges[g->e].t))==2) {
-		g->n=max3(g->n,g->edges[g->e].s,g->edges[g->e].t);
-		if (g->e++==e1) {
-			e1+=NLINKS;
-			g->edges=realloc(g->edges,e1*sizeof(edge));
-		}
-	}
-	fclose(file);
-	g->n++;
+  g->n=0;
+  g->e=0;
+  file=fopen(input,"r");
+  g->edges=malloc(e1*sizeof(edge));
+  while (fscanf(file,"%lu %lu", &(g->edges[g->e].s), &(g->edges[g->e].t))==2) {
+    g->n=max3(g->n,g->edges[g->e].s,g->edges[g->e].t);
+    if (g->e++==e1) {
+      e1+=NLINKS;
+      g->edges=realloc(g->edges,e1*sizeof(edge));
+    }
+  }
+  fclose(file);
+  g->n++;
 
-	g->edges=realloc(g->edges,g->e*sizeof(edge));
+  g->edges=realloc(g->edges,g->e*sizeof(edge));
 
-	return g;
+  return g;
 }
 
 
 //relabel the nodes with contiguous labels from 0 to n-1
 unsigned long* relabel(adjlist* g){
-	unsigned long long i;
-	unsigned long *new=malloc(g->n*sizeof(unsigned long));
-	bool *b=calloc(g->n,sizeof(bool));
+  unsigned long long i;
+  unsigned long *new=malloc(g->n*sizeof(unsigned long));
+  bool *b=calloc(g->n,sizeof(bool));
 
-	g->map=malloc(g->n*sizeof(unsigned long));
-	g->n=0;///////////////
-	for (i=0;i<g->e;i++) {
-		if (b[g->edges[i].s]==0){
-			b[g->edges[i].s]=1;
-			g->map[g->n]=g->edges[i].s;
-			new[g->edges[i].s]=g->n++;
-		}
-		if (b[g->edges[i].t]==0){
-			b[g->edges[i].t]=1;
-			g->map[g->n]=g->edges[i].t;
-			new[g->edges[i].t]=g->n++;
-		}
-	}
-	g->map=realloc(g->map,g->n*sizeof(unsigned long));
-	return new;
+  g->map=malloc(g->n*sizeof(unsigned long));
+  g->n=0;
+  for (i=0;i<g->e;i++) {
+    if (b[g->edges[i].s]==0){
+      b[g->edges[i].s]=1;
+      g->map[g->n]=g->edges[i].s;
+      new[g->edges[i].s]=g->n++;
+    }
+    if (b[g->edges[i].t]==0){
+      b[g->edges[i].t]=1;
+	g->map[g->n]=g->edges[i].t;
+	new[g->edges[i].t]=g->n++;
+      }
+  }
+  g->map=realloc(g->map,g->n*sizeof(unsigned long));
+  return new;
 }
 
 
 //building the adjacency matrix
 void mkadjlist(adjlist* g,unsigned long* new){
-	unsigned long long i;
-	unsigned long u,v;
-	unsigned long *d=calloc(g->n,sizeof(unsigned long));
+  unsigned long long i;
+  unsigned long u,v;
+  unsigned long *d=calloc(g->n,sizeof(unsigned long));
 
-	for (i=0;i<g->e;i++) {
-		g->edges[i].s=new[g->edges[i].s];
-		g->edges[i].t=new[g->edges[i].t];
-	}
+  for (i=0;i<g->e;i++) {
+    g->edges[i].s=new[g->edges[i].s];
+    g->edges[i].t=new[g->edges[i].t];
+  }
 
-	for (i=0;i<g->e;i++) {
-		d[g->edges[i].s]++;
-		d[g->edges[i].t]++;
-	}
+  for (i=0;i<g->e;i++) {
+    d[g->edges[i].s]++;
+    d[g->edges[i].t]++;
+  }
 
-	g->cd=malloc((g->n+1)*sizeof(unsigned long long));
-	g->cd[0]=0;
-	for (i=1;i<g->n+1;i++) {
-		g->cd[i]=g->cd[i-1]+d[i-1];
-		d[i-1]=0;
-	}
+  g->cd=malloc((g->n+1)*sizeof(unsigned long long));
+  g->cd[0]=0;
+  for (i=1;i<g->n+1;i++) {
+    g->cd[i]=g->cd[i-1]+d[i-1];
+    d[i-1]=0;
+  }
 
-	g->adj=malloc(2*g->e*sizeof(unsigned long));
+  g->adj=malloc(2*g->e*sizeof(unsigned long));
 
-	for (i=0;i<g->e;i++) {
-		u=g->edges[i].s;
-		v=g->edges[i].t;
-		g->adj[ g->cd[u] + d[u]++ ]=v;
-		g->adj[ g->cd[v] + d[v]++ ]=u;
-	}
+  for (i=0;i<g->e;i++) {
+    u=g->edges[i].s;
+    v=g->edges[i].t;
+    g->adj[ g->cd[u] + d[u]++ ]=v;
+    g->adj[ g->cd[v] + d[v]++ ]=u;
+  }
 
-	free(d);
+  g->weights = NULL;
+  g->totalWeight = 2*g->e;
+
+
+
+  free(d);
 }
 
 
 
 //freeing memory
 void free_adjlist(adjlist *g){
-	free(g->edges);
-	free(g->cd);
-	free(g->adj);
-	free(g->map);
-	free(g);
+  free(g->edges);
+  free(g->weights);
+  free(g->cd);
+  free(g->adj);
+  free(g->map);
+  free(g);
 }
 
 
 //Make the nlab subgraphs of graph g using the labels "lab"
 adjlist** mkkids(adjlist* g, unsigned long* lab, unsigned long nlab){
-	unsigned long i;
-	unsigned long long j;
+  unsigned long i;
+  unsigned long long j;
 
-	adjlist** clust=malloc(nlab*sizeof(adjlist*));
-	for (i=0;i<nlab;i++){
-		clust[i]=malloc(sizeof(adjlist));
-		clust[i]->edges=malloc(NLINKS*sizeof(edge));
-		clust[i]->e=0;
-		clust[i]->emax=NLINKS2;
-	}
+  adjlist** clust=malloc(nlab*sizeof(adjlist*));
+  for (i=0;i<nlab;i++){
+    clust[i]=malloc(sizeof(adjlist));
+    clust[i]->edges=malloc(NLINKS*sizeof(edge));
+    clust[i]->e=0;
+    clust[i]->emax=NLINKS2;
+  }
 
-	for (j=0;j<g->e;j++) {
-		if (lab[g->edges[j].s]==lab[g->edges[j].t]){
-			i=lab[g->edges[j].s];
-			clust[i]->edges[clust[i]->e]=g->edges[j];
-			if (clust[i]->e++==clust[i]->emax) {
-				clust[i]->emax*=2;
-				clust[i]->edges=realloc(clust[i]->edges,clust[i]->emax*sizeof(edge));
-			}
-		}
-	}
-	for (i=0;i<nlab;i++){
-		clust[i]->edges=realloc(clust[i]->edges,clust[i]->e*sizeof(edge));
-		clust[i]->emax=clust[i]->e;
-		clust[i]->n=0;
-	}
+  for (j=0;j<g->e;j++) {
+    if (lab[g->edges[j].s]==lab[g->edges[j].t]){
+      i=lab[g->edges[j].s];
+      clust[i]->edges[clust[i]->e]=g->edges[j];
+      if (clust[i]->e++==clust[i]->emax) {
+	clust[i]->emax*=2;
+	clust[i]->edges=realloc(clust[i]->edges,clust[i]->emax*sizeof(edge));
+      }
+    }
+  }
+  for (i=0;i<nlab;i++){
+    clust[i]->edges=realloc(clust[i]->edges,clust[i]->e*sizeof(edge));
+    clust[i]->emax=clust[i]->e;
+    clust[i]->n=0;
+  }
 
-	for (i=0;i<g->n;i++){
-		(clust[lab[i]]->n)++;
-	}
+  for (i=0;i<g->n;i++){
+    (clust[lab[i]]->n)++;
+  }
 
-	unsigned long *new=malloc(g->n*sizeof(unsigned long));
-	for (i=0;i<nlab;i++){
-		clust[i]->map=malloc(clust[i]->n*sizeof(unsigned long));
-		clust[i]->n=0;
-	}
+  unsigned long *new=malloc(g->n*sizeof(unsigned long));
+  for (i=0;i<nlab;i++){
+    clust[i]->map=malloc(clust[i]->n*sizeof(unsigned long));
+    clust[i]->n=0;
+  }
 
-	for (i=0;i<g->n;i++){
-		j=lab[i];
-		clust[j]->map[clust[j]->n]=g->map[i];
-		new[i]=(clust[j]->n)++;
-	}
+  for (i=0;i<g->n;i++){
+    j=lab[i];
+    clust[j]->map[clust[j]->n]=g->map[i];
+    new[i]=(clust[j]->n)++;
+  }
 
-	for (i=0;i<nlab;i++){
-		mkadjlist(clust[i],new);
-	}
-	free(new);
-	return clust;
+  for (i=0;i<nlab;i++){
+    mkadjlist(clust[i],new);
+  }
+  free(new);
+  return clust;
 }
 
 
 //recursive function
-void recurs(partition part, adjlist* g, unsigned h, FILE* file){
-	unsigned long nlab;
-	unsigned long i;
-	if (g->e==0){
-		fprintf(file,"%u 1 %lu",h,g->n);
-		for (i=0;i<g->n;i++){
-			fprintf(file," %lu",g->map[i]);
-		}
-		fprintf(file,"\n");
-		free_adjlist(g);
-	}
-	else{
-		unsigned long *lab=malloc(g->n*sizeof(unsigned long));
-		nlab=part(g,lab);
-		if (nlab==1){
-			fprintf(file,"%u 1 %lu",h,g->n);
-			for (i=0;i<g->n;i++){
-				fprintf(file," %lu",g->map[i]);
-			}
-			fprintf(file,"\n");
-			free_adjlist(g);
-			free(lab);
-		}
-		else{
-			adjlist** clust=mkkids(g,lab,nlab);
-			fprintf(file,"%u %lu\n",h,nlab);
-			free_adjlist(g);
-			free(lab);
-			for (i=0;i<nlab;i++){
-				recurs(part, clust[i],h+1,file);
-			}
-		}
-	}
+void recurs(partitionFunction part, adjlist* g, unsigned h, FILE* file){
+  unsigned long nlab;
+  unsigned long i;
+  if (g->e==0){
+    fprintf(file,"%u 1 %lu",h,g->n);
+    for (i=0;i<g->n;i++){
+      fprintf(file," %lu",g->map[i]);
+    }
+    fprintf(file,"\n");
+    free_adjlist(g);
+  }
+  else{
+    unsigned long *lab=malloc(g->n*sizeof(unsigned long));
+    nlab=part(g,lab);
+
+    if (nlab==1){
+      fprintf(file,"%u 1 %lu",h,g->n);
+      for (i=0;i<g->n;i++){
+	fprintf(file," %lu",g->map[i]);
+      }
+      fprintf(file,"\n");
+      free_adjlist(g);
+      free(lab);
+    }
+    else{
+      adjlist** clust=mkkids(g,lab,nlab);
+
+      fprintf(file,"%u %lu\n",h,nlab);
+      free_adjlist(g);
+      free(lab);
+      for (i=0;i<nlab;i++){
+	recurs(part, clust[i],h+1,file);
+      }
+    }
+  }
 }
 
 
 //main function
 int main(int argc,char** argv){
-	adjlist* g;
-	partition part;
-	unsigned long n;
-	unsigned long *map,*new;
+  adjlist* g;
+  partitionFunction part;
+  unsigned long n;
+  unsigned long *map,*new;
 
-	time_t t1=time(NULL),t2;
-	srand(time(NULL));
+  time_t t1=time(NULL),t2;
+  srand(time(NULL));
 
-	if (argc==3)
-		part=choose_partition("0");
-	else if (argc==4)
-		part=choose_partition(argv[3]);
-	else{
-		printf("Command line arguments are not valid.\n");
-		exit(1);
-	}
+  if (argc==3)
+    part=choose_partition("0");
+  else if (argc==4)
+    part=choose_partition(argv[3]);
+  else{
+    printf("Command line arguments are not valid.\n");
+    exit(1);
+  }
 
-	printf("Reading edgelist from file %s\n",argv[1]);
-	g=readedgelist(argv[1]);
+  printf("Reading edgelist from file %s\n",argv[1]);
+  g=readedgelist(argv[1]);
 
-	printf("Building adjacency array\n");
-	new=relabel(g);
-	mkadjlist(g,new);
-	free(new);
-	n=g->n;
+  printf("Building adjacency array\n");
+    new=relabel(g);
+  mkadjlist(g,new);
+  free(new);
+  n=g->n;
 
-	printf("Number of nodes: %lu\n",g->n);
-	printf("Number of edges: %llu\n",g->e);
+  printf("Number of nodes: %lu\n",g->n);
+  printf("Number of edges: %llu\n",g->e);
 
-	printf("Starting recursive bisections\n");
-	printf("Prints result in file %s\n",argv[2]);
-	FILE* file=fopen(argv[2],"w");
-	recurs(part, g, 0, file);
-	fclose(file);
-	t2=time(NULL);
+  printf("Starting recursive bisections\n");
+  printf("Prints result in file %s\n",argv[2]);
+  FILE* file=fopen(argv[2],"w");
+  recurs(part, g, 0, file);
+  fclose(file);
+  t2=time(NULL);
 
-	printf("- Overall time = %ldh%ldm%lds\n",(t2-t1)/3600,((t2-t1)%3600)/60,((t2-t1)%60));
+  printf("- Overall time = %ldh%ldm%lds\n",(t2-t1)/3600,((t2-t1)%3600)/60,((t2-t1)%60));
 
-	return 0;
+  return 0;
 }
 
--- partition.c
+++ partition.c
@@ -1,9 +1,35 @@
 #include "partition.h"
 
 #define K 5
+#define MIN_IMPROVEMENT 0.005
+
+unsigned long randomPartition(adjlist *g,unsigned long *lab);
+unsigned long louvainPartition(adjlist *g, unsigned long *lab);
+
+partitionFunction choose_partition(char *c){
+	printf("Chosen partition algorithm: ");
+	if (strcmp(c,"0")==0){
+		printf("Random partition\n");
+		return randomPartition;
+	}
+	//if (strcmp(c,"1")==0){
+	//	printf("Greedy Sparsest Cut\n");
+	//	return greedySparsestcut;
+	//}
+
+	if (strcmp(c,"2")==0){
+		printf("Louvain partition\n");
+		return louvainPartition;
+	}
+	printf("unknown\n");
+	exit(1);
+}
+
+// -----------------------------------------------------------
+// START Random utility functions
 
 //generating n random labels (boolean values)
-unsigned long init(adjlist *g,unsigned long *lab){
+unsigned long randomPartition(adjlist *g,unsigned long *lab){
 	unsigned long i,n=g->n;
 	unsigned long nlab=(K>n)?n:K;
 
@@ -15,18 +41,264 @@ unsigned long init(adjlist *g,unsigned long *lab){
 	return nlab;
 }
 
-partition choose_partition(char *c){
-	printf("Chosen partition algorithm: ");
-	if (strcmp(c,"0")==0){
-		printf("Random partition\n");
-		return init;
-	}
-	//if (strcmp(c,"1")==0){
-	//	printf("Greedy Sparsest Cut\n");
-	//	return greedySparsestcut;
-	//}
-	printf("unknown\n");
-	exit(1);
+
+// END Random utility functions
+// -----------------------------------------------------------
+
+
+// -----------------------------------------------------------
+// START Louvain utility functions
+
+/*
+int myCompare (const void * a, const void * b, void * array2) {
+  long diff = ((unsigned long *)array2)[*(unsigned long *)a] - ((unsigned long *)array2)[*(unsigned *)b];
+  int res = (0 < diff) - (diff < 0);
+  return  res;
+}
+
+unsigned long * mySort(unsigned long *part, unsigned long size) {
+  unsigned long *nodes = (unsigned long *)malloc(size * sizeof(unsigned long));
+  for (unsigned long i = 0; i < size; i++) {
+    nodes[i]=i;
+  }
+
+  qsort_r(nodes, size, sizeof(unsigned long), myCompare, (void *)part);
+
+  return nodes;
+}
+*/
+
+
+
+void freeLouvainPartition(partition *p) {
+  free(p->in);
+  free(p->tot);
+  free(p->neighCommWeights);
+  free(p->neighCommPos);
+  free(p->node2Community);
+  free(p);
 }
 
 
+partition *createLouvainPartition(adjlist *g) {
+  partition *p = (partition *)malloc(sizeof(partition));
+
+  p->size = g->n;
+
+  p->node2Community = (unsigned long *)malloc(p->size * sizeof(unsigned long));
+  p->in = (long double *)malloc(p->size * sizeof(long double));
+  p->tot = (long double *)malloc(p->size * sizeof(long double));
+
+  p->neighCommWeights = (long double *)malloc(p->size * sizeof(long double));
+  p->neighCommPos = (unsigned long *)malloc(p->size * sizeof(unsigned long));
+  p->neighCommNb = 0;
+
+  for (unsigned long node = 0; node < p->size; node++) {
+    p->node2Community[node] = node;
+    p->in[node]  = selfloopWeighted(g, node);
+    p->tot[node] = degreeWeighted(g, node);
+    p->neighCommWeights[node] = -1;
+    p->neighCommPos[node] = 0;
+  }
+
+  return p;
+}
+
+long double modularity(partition *p, adjlist *g) {
+  long double q  = 0.0L;
+  long double m2 = g->totalWeight;
+
+  for (unsigned long node = 0; node < p->size; node++) {
+    if (p->tot[node] > 0.0L)
+      q += p->in[node] - (p->tot[node] * p->tot[node]) / m2;
+  }
+
+  return q / m2;
+}
+
+void neighCommunitiesInit(partition *p) {
+  for (unsigned long i = 0; i < p->neighCommNb; i++) {
+    p->neighCommWeights[p->neighCommPos[i]] = -1;
+  }
+  p->neighCommNb = 0;
+}
+
+/*
+Computes the set of neighbor communities of a given node (excluding self-loops)
+*/
+void neighCommunities(partition *p, adjlist *g, unsigned long node) {
+  p->neighCommPos[0] = p->node2Community[node];
+  p->neighCommWeights[p->neighCommPos[0]] = 0.;
+  p->neighCommNb = 1;
+
+  // for all neighbors of node, add weight to the corresponding community
+  for (unsigned long long i = g->cd[node]; i < g->cd[node + 1]; i++) {
+    unsigned long neigh  = g->adj[i];
+    unsigned long neighComm = p->node2Community[neigh];
+    long double neighW = (g->weights == NULL)?1.0:g->weights[i];
+
+    // if not a self-loop
+    if (neigh != node) {
+      // if community is new (weight == -1)
+      if (p->neighCommWeights[neighComm] == -1) {
+	p->neighCommPos[p->neighCommNb] = neighComm;
+	p->neighCommWeights[neighComm] = 0.;
+	p->neighCommNb++;
+      }
+      p->neighCommWeights[neighComm] += neighW;
+    }
+  }
+}
+
+/*
+Same behavior as neighCommunities except:
+- self loop are counted
+- data structure if not reinitialised
+*/
+void neighCommunitiesAll(partition *p, adjlist *g, unsigned long node) {
+  for (unsigned long long i = g->cd[node]; i < g->cd[node + 1]; i++) {
+    unsigned long neigh  = g->adj[i];
+    unsigned long neighComm = p->node2Community[neigh];
+    long double neighW = (g->weights == NULL)?1.0:g->weights[i];
+    
+    // if community is new
+    if (p->neighCommWeights[neighComm] == -1) {
+      p->neighCommNb++;
+      p->neighCommPos[p->neighCommNb] = neighComm;
+      p->neighCommWeights[neighComm] = 0.;      
+    }
+    p->neighCommWeights[neighComm] += neighW;
+  }
+}
+
+
+unsigned long updatePartition(partition *p, unsigned long *part, unsigned long size) {
+  // Renumber the communities in p
+  unsigned long *renumber = (unsigned long *)calloc(p->size, sizeof(unsigned long));
+  unsigned long last = 1;
+  for (unsigned node = 0; node < p->size; node++) {
+    if (renumber[p->node2Community[node]] == 0) {
+      renumber[p->node2Community[node]] = last++;
+    }
+  }
+
+  // Update part with the renumbered communities in p
+  for (unsigned long i = 0; i < size; i++) {
+    part[i] = renumber[p->node2Community[part[i]]] - 1;
+  }
+
+  free(renumber);
+  return last-1;
+}
+
+
+// Compute one pass of Louvain and returns the improvement
+long double louvainOneLevel(partition *p, adjlist *g) {
+  unsigned long nbMoves;
+  long double startModularity = modularity(p, g);
+  long double newModularity = startModularity;
+  long double curModularity;
+
+  // generate a random order for nodes' movements
+  /*
+  unsigned long *randomOrder = (unsigned long *)malloc(p->size * sizeof(unsigned long));
+  for (unsigned long i = 0; i < p->size; i++)
+    randomOrder[i] = i;
+  for (unsigned long i = 0; i < p->size - 1; i++) {
+    unsigned long randPos = rand()%(p->size - i) + i;
+    unsigned long  tmp = randomOrder[i];
+    randomOrder[i] = randomOrder[randPos];
+    randomOrder[randPos] = tmp;
+  }
+  */
+  
+  // repeat while 
+  //   there are some nodes moving
+  //   or there is an improvement of quality greater than a given epsilon 
+  do {
+    curModularity = newModularity;
+    nbMoves = 0;
+
+    // for each node:
+    //   remove the node from its community
+    //   compute the gain for its insertion in all neighboring communities
+    //   insert it in the best community with the highest gain
+    for (unsigned long i = 0; i < g->n; i++) {
+      unsigned long node = i;//randomOrder[nodeTmp];
+      unsigned long oldComm = p->node2Community[node];
+      long double degreeW = degreeWeighted(g, node);
+
+
+      // computation of all neighboring communities of current node
+      neighCommunitiesInit(p);
+      neighCommunities(p, g, node);
+
+      // remove node from its current community
+      removeNode(p, g, node, oldComm, p->neighCommWeights[oldComm]);
+
+      // compute the gain for all neighboring communities
+      // default choice is the former community
+      unsigned long bestComm = oldComm;
+      long double bestCommW  = 0.0L;
+      long double bestGain = 0.0L;
+      for (unsigned long i = 0; i < p->neighCommNb; i++) {
+	unsigned long newComm = p->neighCommPos[i];
+	long double newGain = gain(p, g, newComm, p->neighCommWeights[newComm], degreeW);
+
+	if (newGain > bestGain) {
+	  bestComm = newComm;
+	  bestCommW = p->neighCommWeights[newComm];
+	  bestGain = newGain;
+	}
+      }
+
+      // insert node in the nearest community
+      insertNode(p, g, node, bestComm, bestCommW);
+
+      if (bestComm != oldComm) {
+	nbMoves++;
+      }
+    }
+
+    newModularity = modularity(p, g);
+  } while (nbMoves>0 && 
+    	   newModularity - curModularity > MIN_IMPROVEMENT);
+  
+  //  free(randomOrder);
+
+  return newModularity - startModularity;
+}
+
+unsigned long louvainPartition(adjlist *g, unsigned long *lab) {
+  // Initialize partition with trivial communities
+  for (unsigned long i = 0; i < g->n; i++) {
+    lab[i] = i;
+  }
+  
+  // Execution of Louvain method
+  //  while(1) {
+    partition *gp = createLouvainPartition(g);
+    long double improvement = louvainOneLevel(gp, g);
+    //    if (improvement < MIN_IMPROVEMENT) {
+    //      freeLouvainPartition(gp);
+    //      break;
+    //    }
+    
+    unsigned long n = updatePartition(gp, lab, g->n);
+    
+
+    //adjlist *g2 = partition2Graph(gp, g);
+    //    if (g->n < size) {
+    //      freeGraph(g);
+    //    }
+    freeLouvainPartition(gp);
+    //    g = g2;
+    //  }
+
+  return n;
+}
+
+
+
+// END Louvain utility functions
+// -----------------------------------------------------------
